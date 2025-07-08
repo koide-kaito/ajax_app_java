8f01d6a46d8949a0589866c0f1eb260a1d5fade9
koide-kaito <koide.kaito@minebea-ss.com>
2025-07-08 10:37:12 +0900

    JSでレスポンスを処理する

--- src/main/resources/static/js/memo.js
+++ src/main/resources/static/js/memo.js
@@ -1,5 +1,20 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.createdAt}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html;
+};
+
+
 function post (){
- //リクエストを送信する処理
+//  リクエストを送信する処理
 //  console.log("イベント発火");
   const submit = document.getElementById("submit");
 
@@ -12,9 +27,30 @@ function post (){
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
-    
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.response.error}`);
+        return null;
+      };
+      // console.log(XHR.response);
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+      // console.log(formText.value);
+      // const item = XHR.response;
+      // const html = `
+      //   <div class="post">
+      //     <div class="post-date">
+      //       投稿日時：${item.createdAt}
+      //     </div>
+      //     <div class="post-content">
+      //       ${item.content}
+      //     </div>
+      //   </div>`;
+      // list.insertAdjacentHTML("afterend", html);
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = "";
+    };
   });
-
 };
 
 window.addEventListener('load', post);
